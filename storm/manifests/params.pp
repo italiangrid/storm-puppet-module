284103bf2366fca166c6d9f7004da6a741d9b9fb
enricovianello <maverick17586@gmail.com>
2019-03-25 15:23:40 +0100

    Use rake to launch tests

--- storm/manifests/params.pp
+++ storm/manifests/params.pp
@@ -1,3 +1,8 @@
+# Class: storm::params
+# ===========================
+#
+# storm class default parameters
+#
 class storm::params {
   $storm_user_name = 'storm'
   $storm_storage_root_directory = '/storage'
