a728e9a403dafc086783a8c7786772744b59b8ca
enricovianello <maverick17586@gmail.com>
2019-03-28 10:40:26 +0100

    More generic spec_helper

--- storm/spec/spec_helper.rb
+++ storm/spec/spec_helper.rb
@@ -1,12 +1,28 @@
+if ENV['COVERAGE'] == 'yes'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    add_filter(/^\/spec\//)
+  end
+end
+
 require 'rspec-puppet'
 
-fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
+# rspec 2.x doesn't have RSpec::Support, so fall back to File::ALT_SEPARATOR to
+# detect if running on windows
+def windows?
+  return @windowsp unless @windowsp.nil?
+  @windowsp = defined?(RSpec::Support) ? RSpec::Support::OS.windows? : !!File::ALT_SEPARATOR
+end
 
 RSpec.configure do |c|
-  c.module_path     = File.join(fixture_path, 'modules')
-  c.manifest_dir    = File.join(fixture_path, 'manifests')
-  c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
+  c.module_path     = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'modules')
+  c.manifest_dir    = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'manifests')
+  c.manifest        = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'manifests', 'site.pp')
   c.environmentpath = File.join(Dir.pwd, 'spec')
+  c.parser          = ENV['FUTURE_PARSER'] == 'yes' ? 'future' : 'current'
 
   c.after(:suite) do
     RSpec::Puppet::Coverage.report!(80)
