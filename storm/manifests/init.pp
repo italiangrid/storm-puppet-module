5618fa4603012c15911c98cd09d2cb0cb4b8d3bc
enricovianello <maverick17586@gmail.com>
2019-05-15 17:46:04 +0200

    Doc fixed

--- storm/manifests/init.pp
+++ storm/manifests/init.pp
@@ -1,46 +1,44 @@
 # Class: storm
 # ===========================
 #
-# Full description of class storm here.
+# StoRM official puppet module
 #
 # Parameters
 # ----------
 #
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
+# * `storm_user_name`
+# Specify StoRM Unix user as a string.
+# * `storm_storage_root_directory`
+# Specify Storage Areas root directory path as a string.
 #
 # Examples
 # --------
 #
 # @example
 #    class { 'storm':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#      storm_user_name => 'storm',
+#      storm_storage_root_directory => '/storage',
 #    }
 #
 # Authors
 # -------
 #
-# Author Name <author@domain.com>
+# Enrico Vianello <enrico.vianello@cnaf.infn.it>
 #
 # Copyright
 # ---------
 #
-# Copyright 2019 Your name here, unless otherwise noted.
+# Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2006-2010.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
 #
 class storm (
   $storm_user_name              = storm::params::storm_user_name,
