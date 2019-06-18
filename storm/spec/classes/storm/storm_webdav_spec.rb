0e7410867faa88316e0505b449d7978dbc1e34fb
enricovianello <maverick17586@gmail.com>
2019-06-18 10:17:33 +0200

    Fixed webdav test

--- storm/spec/classes/storm/storm_webdav_spec.rb
+++ storm/spec/classes/storm/storm_webdav_spec.rb
@@ -41,7 +41,7 @@ describe 'storm::webdav' do
         end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_package('storm-webdav').with(ensure: 'present') }
+        it { is_expected.to contain_package('storm-webdav').with(ensure: 'installed') }
         it { is_expected.to contain_service('storm-webdav').with(ensure: 'running') }
   
         it "check storm webdav configuration directory" do
