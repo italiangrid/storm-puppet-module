e74ac6a40a35005a708cc8a109b16a9d781ba6dd
enricovianello <maverick17586@gmail.com>
2020-03-13 18:24:18 +0100

    Fixed image tag

--- docker/ci/push-image-dockerhub.sh
+++ docker/ci/push-image-dockerhub.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -ex
 
-if [ -z ${BRANCH_NAME+x} ]; then
-  echo "BRANCH_NAME is unset"
-  BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-else
-  echo "BRANCH_NAME is set to '$BRANCH_NAME'"
-fi
-
-echo "Pushing italiangrid/storm-testsuite:${BRANCH_NAME} on dockerhub ..."
-docker push italiangrid/storm-testsuite:${BRANCH_NAME}
+echo "Pushing italiangrid/docker-rspec-puppet:ci on dockerhub ..."
+docker push italiangrid/docker-rspec-puppet:ci
